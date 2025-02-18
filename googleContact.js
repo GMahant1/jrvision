bb41bc5259962b9d4f5beecd03526526776d3410
Gaurav Mahant <thegau22@gmail.com>
2025-02-17 20:05:07 -0500

    rough draft of P+ driven event creation in google

---
+++ googleContact.js
@@ -0,0 +1,7 @@
+// fetch appointments from P+
+if (appointment.type = new || userExist!) {
+  // shape profile data from P+
+  // send to googleContact API
+} else {
+  // create a event in Google Calendar with info
+}
