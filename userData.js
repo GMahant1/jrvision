a39cf9bd541c58e232a68f9c61bb49f400bef9c2
Gaurav Mahant <thegau22@gmail.com>
2025-02-17 20:03:58 -0500

    function returns the full internal profile of 1 nexTech patient , search by first+last+dob

---
+++ userData.js
@@ -0,0 +1,31 @@
+const getAccessToken = require('./Auth'); // Import getAccessToken from auth.js
+require('dotenv').config();
+const axios = require("axios");
+
+const NX_PRACTICE_ID = process.env.NX_PRACTICE_ID;
+
+const userData = async (firstName, lastName, birthDate) => {
+  try {
+    // Get the auth token
+    const token = await getAccessToken();
+
+    // Build Query
+    const endpoint = `https://api.pm.nextech.com/api/Patient?family=${lastName}&given=${firstName}&birthdate=eq${birthDate}`;
+
+    // Make the GET request with the token
+    const response = await axios.get(endpoint, {
+      headers: {
+        "Authorization": `Bearer ${token}`, // Pass the token in the Authorization header
+        "nx-practice-id": NX_PRACTICE_ID,
+      },
+    });
+
+    return response.data
+  } catch (error) {
+    console.error("Error fetching data with token:", error);
+    throw error;
+  }
+};
+
+
+module.exports = userData;
